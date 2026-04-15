16b2d0486401793beb5229d63e72c4b4f3a5d709
Oliver <ylin73505@gmail.com>
2026-04-15 17:06:56 +0800

    建立 angular 專案

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class App {
+  protected readonly title = signal('mcu-114-2-14130434-final');
+}
