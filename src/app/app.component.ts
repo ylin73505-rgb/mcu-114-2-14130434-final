febd15f7700ff1d1927c5d42ca2b269ab23800ae
Oliver <ylin73505@gmail.com>
2026-05-16 22:25:10 +0800

    卡片並排設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from './product-card/product-card.component';
 
+interface Product {
+  title: string;
+  authors: string;
+  publisher: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-root',
   imports: [ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class App {}
+export class App {
+  products: Product[] = [
+    { title: 'A 產品', authors: '作者 A、作者 B、作者 C', publisher: '博碩文化', price: 1580 },
+    { title: 'B 產品', authors: '作者 A、作者 B、作者 C', publisher: '博碩文化', price: 1580 },
+    { title: 'C 產品', authors: '作者 A、作者 B、作者 C', publisher: '博碩文化', price: 1580 },
+    { title: 'D 產品', authors: '作者 A、作者 B、作者 C', publisher: '博碩文化', price: 1580 },
+    { title: 'E 產品', authors: '作者 A、作者 B、作者 C', publisher: '博碩文化', price: 1580 },
+  ];
+}
