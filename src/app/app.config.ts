f90b552910b0f43b368a15a67b80e5070b92cd56
Oliver <ylin73505@gmail.com>
2026-05-19 18:39:33 +0800

    12

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), { provide: LocationStrategy, useClass: HashLocationStrategy }, provideRouter(routes)],
 };
